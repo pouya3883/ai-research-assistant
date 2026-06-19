6c1b4a6aff02f097b2ea3a8774c33a70caea61fd
Pouya <pouya3883@gmail.com>
2026-06-19 19:39:40 +0400

    feat: add embedding generation and registry

--- app/services/embedding_registry.py
+++ app/services/embedding_registry.py
@@ -12,3 +12,25 @@ def load_embeddings():
 def save_embeddings(embeddings):
     with open(EMBEDDINGS_FILE, "w", encoding="utf-8") as f:
         json.dump(embeddings, f, indent=4)
+
+
+def add_embedding(embedding):
+    embeddings = load_embeddings()
+
+    for i in embeddings:
+        if i["chunk_filename"] == embedding.chunk_filename:
+            return
+
+    embeddings.append(embedding.model_dump())
+
+    save_embeddings(embeddings)
+
+
+def get_document_embeddings(document_id: str):
+    embeddings = load_embeddings()
+
+    results = [
+        embedding for embedding in embeddings if embedding["document_id"] == document_id
+    ]
+
+    return results
--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,7 +1,29 @@
 from app.models.embedding import DocumentEmbedding
+from sentence_transformers import SentenceTransformer
+from app.services.chunk_service import get_document_chunks
+from app.services.embedding_registry import add_embedding
+
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
 
 def create_embedding(document_id: str, chunk_filename: str, content: str):
+    embedding_vector = embedding_model.encode(content).tolist()
+
     return DocumentEmbedding(
-        document_id=document_id, chunk_filename=chunk_filename, embedding=[]
+        document_id=document_id,
+        chunk_filename=chunk_filename,
+        embedding=embedding_vector,
     )
+
+
+def generate_document_embeddings(document_id: str):
+    chunks = get_document_chunks(document_id)
+
+    for chunk in chunks:
+        embedding = create_embedding(
+            document_id=document_id,
+            chunk_filename=chunk["filename"],
+            content=chunk["content"],
+        )
+
+        add_embedding(embedding)
